5ace8e34b504c1ea7d609d415c5fdeece0cd9575
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-09-18 13:43:17 +0530

    code updated

--- android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
+++ android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
@@ -3,12 +3,6 @@
 // found in the LICENSE file.
 
 package io.flutter.plugins.videoplayer;
-<<<<<<< HEAD
-import androidx.media3.common.AudioAttributes;
-import androidx.media3.common.Player;
-=======
-
->>>>>>> refs/remotes/origin/main
 import static androidx.media3.common.Player.REPEAT_MODE_ALL;
 import static androidx.media3.common.Player.REPEAT_MODE_OFF;
 
@@ -17,34 +11,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RestrictTo;
 import androidx.annotation.VisibleForTesting;
-<<<<<<< HEAD
-import androidx.media3.common.C;
-import androidx.media3.common.Format;
-import androidx.media3.common.MediaItem;
-import androidx.media3.common.PlaybackException;
-import androidx.media3.common.PlaybackParameters;
-import androidx.media3.common.Player.Listener;
-import androidx.media3.datasource.DataSource;
-import androidx.media3.datasource.DefaultDataSource;
-import androidx.media3.datasource.DefaultHttpDataSource;
-import androidx.media3.exoplayer.source.MediaSource;
-import androidx.media3.exoplayer.source.ProgressiveMediaSource;
-import androidx.media3.exoplayer.dash.DashMediaSource;
-import androidx.media3.exoplayer.dash.DefaultDashChunkSource;
-import androidx.media3.exoplayer.hls.HlsMediaSource;
-import androidx.media3.exoplayer.smoothstreaming.DefaultSsChunkSource;
-import androidx.media3.exoplayer.smoothstreaming.SsMediaSource;
-import androidx.media3.common.util.Util;
-import androidx.media3.exoplayer.ExoPlayer;
-
-import io.flutter.plugin.common.EventChannel;
-=======
 import androidx.media3.common.AudioAttributes;
 import androidx.media3.common.C;
 import androidx.media3.common.MediaItem;
 import androidx.media3.common.PlaybackParameters;
 import androidx.media3.exoplayer.ExoPlayer;
->>>>>>> refs/remotes/origin/main
 import io.flutter.view.TextureRegistry;
 
 final class VideoPlayer implements TextureRegistry.SurfaceProducer.Callback {
@@ -133,102 +104,8 @@ final class VideoPlayer implements TextureRegistry.SurfaceProducer.Callback {
     exoPlayer.setMediaItem(mediaItem);
     exoPlayer.prepare();
 
-<<<<<<< HEAD
-    setUpVideoPlayer(exoPlayer, new QueuingEventSink());
-  }
-
-  // Constructor used to directly test members of this class.
-  @VisibleForTesting
-  VideoPlayer(
-      ExoPlayer exoPlayer,
-      EventChannel eventChannel,
-      TextureRegistry.SurfaceTextureEntry textureEntry,
-      VideoPlayerOptions options,
-      QueuingEventSink eventSink,
-      DefaultHttpDataSource.Factory httpDataSourceFactory) {
-    this.eventChannel = eventChannel;
-    this.textureEntry = textureEntry;
-    this.options = options;
-    this.httpDataSourceFactory = httpDataSourceFactory;
-
-    setUpVideoPlayer(exoPlayer, eventSink);
-  }
-
-  @VisibleForTesting
-  public void buildHttpDataSourceFactory(@NonNull Map<String, String> httpHeaders) {
-    final boolean httpHeadersNotEmpty = !httpHeaders.isEmpty();
-    final String userAgent =
-        httpHeadersNotEmpty && httpHeaders.containsKey(USER_AGENT)
-            ? httpHeaders.get(USER_AGENT)
-            : "ExoPlayer";
-
-    httpDataSourceFactory.setUserAgent(userAgent).setAllowCrossProtocolRedirects(true);
-
-    if (httpHeadersNotEmpty) {
-      httpDataSourceFactory.setDefaultRequestProperties(httpHeaders);
-    }
-  }
-
-  private MediaSource buildMediaSource(
-      Uri uri, DataSource.Factory mediaDataSourceFactory, String formatHint) {
-    int type;
-    if (formatHint == null) {
-      type = Util.inferContentType(uri);
-    } else {
-      type = switch (formatHint) {
-        case FORMAT_SS -> C.CONTENT_TYPE_SS;
-        case FORMAT_DASH -> C.CONTENT_TYPE_DASH;
-        case FORMAT_HLS -> C.CONTENT_TYPE_HLS;
-        case FORMAT_OTHER -> C.CONTENT_TYPE_OTHER;
-        default -> -1;
-      };
-    }
-    switch (type) {
-      case C.CONTENT_TYPE_SS -> {
-        return new SsMediaSource.Factory(
-                new DefaultSsChunkSource.Factory(mediaDataSourceFactory), mediaDataSourceFactory)
-                .createMediaSource(MediaItem.fromUri(uri));
-      }
-      case C.CONTENT_TYPE_DASH -> {
-        return new DashMediaSource.Factory(
-                new DefaultDashChunkSource.Factory(mediaDataSourceFactory), mediaDataSourceFactory)
-                .createMediaSource(MediaItem.fromUri(uri));
-      }
-      case C.CONTENT_TYPE_HLS -> {
-        return new HlsMediaSource.Factory(mediaDataSourceFactory)
-                .createMediaSource(MediaItem.fromUri(uri));
-      }
-      case C.CONTENT_TYPE_OTHER -> {
-        return new ProgressiveMediaSource.Factory(mediaDataSourceFactory)
-                .createMediaSource(MediaItem.fromUri(uri));
-      }
-      default -> throw new IllegalStateException("Unsupported type: " + type);
-    }
-  }
-
-  private void setUpVideoPlayer(ExoPlayer exoPlayer, QueuingEventSink eventSink) {
-    this.exoPlayer = exoPlayer;
-    this.eventSink = eventSink;
-
-    eventChannel.setStreamHandler(
-        new EventChannel.StreamHandler() {
-          @Override
-          public void onListen(Object o, EventChannel.EventSink sink) {
-            eventSink.setDelegate(sink);
-          }
-
-          @Override
-          public void onCancel(Object o) {
-            eventSink.setDelegate(null);
-          }
-        });
-
-    surface = new Surface(textureEntry.surfaceTexture());
-    exoPlayer.setVideoSurface(surface);
-=======
     exoPlayer.setVideoSurface(surfaceProducer.getSurface());
     exoPlayer.addListener(new ExoPlayerEventListener(exoPlayer, videoPlayerEvents));
->>>>>>> refs/remotes/origin/main
     setAudioAttributes(exoPlayer, options.mixWithOthers);
 
     return exoPlayer;
